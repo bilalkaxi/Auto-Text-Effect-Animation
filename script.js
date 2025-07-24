368d5aa91c5abb11174ce3dee3bb6d645ee5988b
Qazi Muhammad Bilal Jamil <129346837+bilalkaxi@users.noreply.github.com>
2025-07-24 14:00:18 +0500

    Add files via upload
    Auto-Text-Effect-Animation

---
+++ script.js
@@ -0,0 +1,25 @@
+const containerEL = document.querySelector(".container");
+
+const careers = ["Frontend-Developer", "Web Developer", "Freelancer", "Instructor"];
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+updateText();
+
+function updateText(){
+    characterIndex++;
+    containerEL.innerHTML = ` <h1> I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+
+
+if (characterIndex === careers[careerIndex].length){
+    careerIndex++;
+    characterIndex= 0;
+}
+
+if (careerIndex === careers.length){
+    careerIndex = 0;
+}
+
+setTimeout(updateText, 400);
+}
